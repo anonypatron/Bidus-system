85dbecd932d14507447db9e8d16207e8cf2f5b7c
rainerhour <rainerhour0904@gmail.com>
2025-11-27 15:00:46 +0900

    Modify Spring loadbalancer

--- bidus-backend/web-bff-service/src/main/java/com/web/bff/config/WebClientConfig.java
+++ bidus-backend/web-bff-service/src/main/java/com/web/bff/config/WebClientConfig.java
@@ -1,6 +1,5 @@
 package com.web.bff.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +9,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+//    @LoadBalanced *** eks환경에서는 k8s가 자동으로 로드밸런서역할을 함. 배포 환경에서는 지우기
     @Bean
-    @LoadBalanced
     @Primary
     public WebClient webClient(ReactorLoadBalancerExchangeFilterFunction lbFunction) {
         return WebClient.builder()
