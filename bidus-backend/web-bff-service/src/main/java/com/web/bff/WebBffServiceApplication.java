009f5b651a684ca9e8f39d5f3f211d88e23ce479
rainerhour <rainerhour0904@gmail.com>
2025-11-28 12:12:30 +0900

    Modify Loadbalancer config

--- bidus-backend/web-bff-service/src/main/java/com/web/bff/WebBffServiceApplication.java
+++ bidus-backend/web-bff-service/src/main/java/com/web/bff/WebBffServiceApplication.java
@@ -2,9 +2,7 @@ package com.web.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class WebBffServiceApplication {
     public static void main(String[] args) {
